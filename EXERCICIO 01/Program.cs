b2ee9da24bbafae346061dc22499e19e948cb155
Igor Mendes <igor.mendes.3939503@gmail.com>
2022-11-21 21:34:44 -0400

    Add files via upload

---
+++ EXERCICIO 01/Program.cs
@@ -0,0 +1,15 @@
+﻿// EXERCICIO 4.4.1
+double n1, n2, n3, n4 = 0;
+double t =0;
+
+Console.WriteLine("Digite 4 numeros: ");
+n1 = Convert.ToDouble(Console.ReadLine());
+n2 = Convert.ToDouble(Console.ReadLine());
+n3 = Convert.ToDouble(Console.ReadLine());
+n4 = Convert.ToDouble(Console.ReadLine());
+
+t = n1 + n2 + n3 + n4;
+
+Console.WriteLine("soma dos 4 numeros e de: " + t);
+
+Console.ReadKey();
