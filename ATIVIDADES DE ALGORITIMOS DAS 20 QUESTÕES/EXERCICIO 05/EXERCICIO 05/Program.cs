40f5095351a130b00118523acaebe8c90a1e4ad9
Igor Mendes <igor.mendes.3939503@gmail.com>
2022-11-21 22:07:45 -0400

    Add files via upload

---
+++ ATIVIDADES DE ALGORITIMOS DAS 20 QUESTÕES/EXERCICIO 05/EXERCICIO 05/Program.cs
@@ -0,0 +1,19 @@
+﻿// EXERCICIO 4.4.5
+
+string p; // produto
+double vp; // valor do produto
+double vt; // Valor total do produto
+double de = 10; // Desconto
+
+
+Console.WriteLine("Digite o Produto desejado");
+p = Console.ReadLine();
+Console.WriteLine("Digite o valor do produto");
+vp = Convert.ToDouble(Console.ReadLine());
+
+vt = vp - (vp * (de / 100));
+
+Console.WriteLine("O valor do produto é: R$" + vp);
+Console.WriteLine("O valor do produto com 10% de desconto é: R$ " + vt);
+
+Console.ReadKey();
