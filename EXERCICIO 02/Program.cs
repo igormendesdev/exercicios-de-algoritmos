299afac3394d6e3dcbe3ce20b3597db20962dd5f
Igor Mendes <igor.mendes.3939503@gmail.com>
2022-11-21 21:36:20 -0400

    Add files via upload

---
+++ EXERCICIO 02/Program.cs
@@ -0,0 +1,16 @@
+﻿//EXERCICIO 4.4.2
+
+double n1, n2, n3 = 0;
+double t = 0;
+double m = 0;
+
+
+Console.WriteLine("Digite 03 numeros para saber a media entre eles: ");
+n1 = Convert.ToDouble(Console.ReadLine());
+n2 = Convert.ToDouble(Console.ReadLine());
+n3 = Convert.ToDouble(Console.ReadLine());
+
+t = n1 + n2 + n3;
+m = t / 3;
+Console.WriteLine("Essa e media dos numeros " + m);
+Console.ReadKey();
