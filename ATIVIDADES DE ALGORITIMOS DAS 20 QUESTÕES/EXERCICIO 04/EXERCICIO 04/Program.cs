73d59dd2ef1dbc7697c00b020a6d2c5cce2506e4
Igor Mendes <igor.mendes.3939503@gmail.com>
2022-11-21 22:03:41 -0400

    Add files via upload

---
+++ ATIVIDADES DE ALGORITIMOS DAS 20 QUESTÕES/EXERCICIO 04/EXERCICIO 04/Program.cs
@@ -0,0 +1,20 @@
+﻿//EXERCICIO 4.4.4
+
+double s = 0; // salario do funcionario
+double a = 8.75;//ajuste salarial
+string n = ""; // nome do funcionario
+double st = 0; // salario total
+double re = 0; // valor do reajuste
+
+Console.WriteLine("Digite o nome do funcionario");
+n = Console.ReadLine();
+Console.WriteLine("Digite o salario do funcionario");
+s = double.Parse(Console.ReadLine());
+
+re= (a/100) * s;
+st = re + s;
+Console.WriteLine("o salario do funcionario é: " + s);
+Console.WriteLine("o salario depois do reajuste é: " + st);
+
+Console.ReadKey();
+
